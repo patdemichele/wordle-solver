22d2f4567b565e70975ca1868fa0fd0c3df043a2
Patrick DeMichele <demichel@Patricks-MacBook-Pro-2.local>
2022-01-13 20:10:04 -0600

    first attempt at adding official wordle lists, and giving more command line options

--- wordle.py
+++ wordle.py
@@ -1,5 +1,10 @@
 from wordfreq import get_frequency_dict
 import numpy as np
+import sys
+
+WORDLE_GUESSES_FILENAME = "guess.txt"
+WORDLE_ANSWERS_FILENAME = "answers.txt"
+
 
 # distribution size such that beyond this size we "compactify" the distribution by sampling it, in certain cases.
 MAX_DISTRIBUTION_SIZE = 200
@@ -27,13 +32,6 @@ def compact_distribution(distribution):
   for c in choices:
     result[c] = result.get(c, 0) + 1.0/MAX_DISTRIBUTION_SIZE
   return result
-
-# get the distribution of length-5 words we want to use
-
-all_words = get_frequency_dict('en', wordlist='small')
-five_dict = {k: all_words[k] for k in all_words if len(k) == 5}
-five_dict = normalize(five_dict)
-print(len(five_dict))
 
 # used in get_coloring_from_guess
 def get_hist(word):
@@ -104,7 +102,7 @@ def compute_new_entropy(candidates, true_word, guess):
   return compute_entropy(pruned)
 
 # candidates: a dict of probabilities. should sum to 1.
-def get_best_guesses(candidates):
+def get_best_guesses(candidates, allowed_guesses, exhaustive=False):
 
   current_entropy = compute_entropy(candidates)
   expected_entropy_reduction = {k: 0 for k in candidates}
@@ -113,10 +111,15 @@ def get_best_guesses(candidates):
   # use sampling
   iter_num = 0
   data = compact_distribution(candidates)
+  if exhaustive:
+    guesses_to_check = allowed_guesses
+  else:
+    # typical case: just check a sampling of guesses.
+    guesses_to_check = [guess for in guess in data if guess in guesses_to_check]
   for true_word in data:
     p_true_word = data[true_word]
     iter_num += 1
-    for guess in data:
+    for guess in guesses_to_check:
       entropy_reduction = current_entropy - compute_new_entropy(candidates, true_word, guess)
       # in computing expected value, need to weight this by the true word probability
       # use 1/N because we are sampling
@@ -125,7 +128,7 @@ def get_best_guesses(candidates):
   topk = min(len(expected_entropy_reduction), 5)
   print(expected_entropy_reduction)
   return sorted(expected_entropy_reduction, key=expected_entropy_reduction.get, reverse=True)[:topk]
-
+  #return sorted(data, key=data.get, reverse=True)[:topk]
 
 def coloring_from_string(coloring_string):
   if len(coloring_string) != 5:
@@ -138,7 +141,80 @@ def coloring_from_string(coloring_string):
   return result
 
 
+# get the distribution of length-5 words we want to use
+def get_wordfreq_distribution():
+  all_words = get_frequency_dict('en', wordlist='small')
+  five_dict = {k: all_words[k] for k in all_words if len(k) == 5}
+  return normalize(five_dict)
+
+def get_custom_distribution(path):
+  d = {}
+  # file can either be line separated words (then they are all weighted equally), or word weight\n word weight\n, etc
+  done_first_line = False
+  weights = False
+  with f as open(path, 'r'):
+    for line in f:
+      sp = line.split(" ")
+      if len(sp) == 2:
+        if done_first_line and not weights:
+          return None
+        word = sp[0]
+        weight = float(sp[1])
+        d[word] = weight
+        weights = True
+      elif len(sp) == 1:
+        if done_first_line and weights:
+          return None
+        word = sp[0]
+        d[word] = 1.0 # will be normalized
+        weights = False
+      else:
+        return None # bad line
+      done_first_line = True
+  return d
+
+def get_wordle_uniform_distribution():
+  return get_custom_distribution(WORDLE_ANSWERS_FILENAME)
+
+def load_guesses_from_file(guesses_filename):
+  allowed_guesses = set()
+  f = open(guesses_filename, 'r')
+  for line in f:
+    guess = line.strip().lower()
+    allowed_guesses.add(guess)
+  f.close()
+  return allowed_guesses
+
+
+def report_usage_and_exit():
+  print("Usage:\nEither `python3 wordle.py english`, `python3 wordle.py uniform`, or `python3 wordle.py custom path/to/prior_distribution.txt`")
+  print("See Github for instructions.")
+  exit(0)
+
 if __name__ == "__main__":
+
+  print("Parsing command line arguments and loading distribution...")
+
+  # parse command line arguments
+  if len(sys.argv) < 2:
+    report_usage_and_exit()
+  distribution_option = sys.argv[1]
+  if distribution_option == "english":
+    answer_distribution = get_wordfreq_distribution()
+  elif distribution_option == "uniform":
+    answer_distribution = get_wordle_uniform_distribution()
+  elif distribution_option == "custom":
+    if len(sys.argv) < 3:
+      report_usage_and_exit()
+    answer_distribution = get_custom_distribution(sys.argv[2])
+  else:
+    report_usage_and_exit()
+
+  allowed_guesses = load_guesses_from_file(WORDLE_GUESSES_FILENAME)
+
+  exhaustive = "--exhaustive" in sys.argv
+  compute_first_guess = "--compute-first-guess" in sys.argv
+
   print("Welcome! This Wordle utility will suggest a guess for you on each round.")
   print("After submitting to Wordle, please type the coloring of that guess and hit enter.")
   print("Write 0 for black, 1 for gold, and 2 for green. So, you may type 01002[enter], e.g..")
@@ -146,18 +222,17 @@ if __name__ == "__main__":
   print("......................")
   print(" ")
 
-  candidates = five_dict.copy()
   round_number = 1
   while True:
     print("ROUND ", round_number)
-    guess = "tears"
-    if round_number == 1:
+    guess = "heart"
+    if round_number == 1 and not compute_first_guess:
       print("It is Round 1, which means that you have zero information from the Wordle board.")
-      print("Assuming Wordle uses words at a frequency similar to spoken English, you should guess `tears`.")
+      print("Assuming Wordle uses words at a frequency similar to spoken English, you should guess `heart`.")
     else:
-        print("Total candidate words = ", len(candidates))
+        print("Total candidate words = ", len(answer_distribution))
         print("Thinking...")
-        guesses = get_best_guesses(candidates)
+        guesses = get_best_guesses(answer_distribution, allowed_guesses, exhaustive)
         print("Here are your recommended guesses, best guess first: ", guesses)
         print("Please type the guess you ended up actually using (lowercase, no quotes). ")
         guess = input("")
@@ -172,12 +247,12 @@ if __name__ == "__main__":
       print("Nice!")
       break
     # prune candidates. approximate=False because we want to include every word
-    candidates = prune_candidates(candidates, guess, coloring, approximate=False)
-    if len(candidates) == 1:
-      print("Only one candidate left: you should guess ", [x for x in candidates][0])
+    answer_distribution = prune_candidates(answer_distribution, guess, coloring, approximate=False)
+    if len(answer_distribution) == 1:
+      print("Only one candidate left: you should guess ", [x for x in answer_distribution][0])
       break
-    if not candidates:
-      print("Oh no! Wordle has a word in mind that is not in our dictionary.")
+    if not answer_distribution:
+      print("Oh no! Wordle has a word in mind that is not supported in the prior distribution.")
       break
     print("...........")
     print(" ")
